90a597052847f363e37a0038598dfef9ed976fe8
Charlotte Gale <CharlotteMGale@outlook.com>
2026-01-29 11:22:16 +0000

    attempts fancy AnimalTests again - doesn't work still, will look into more later

--- Ecosystem.Tests/AnimalTests.cs
+++ Ecosystem.Tests/AnimalTests.cs
@@ -5,7 +5,6 @@ namespace Ecosystem.Tests;
 public class AnimalTests<TAnimal> where TAnimal : Animal, new()
 {
     [Test]
-    [TestCase]
     public void IncreaseHunger_WhenCalled_IncrementsHungerLevel()
     {
         TAnimal animal = new TAnimal();
@@ -14,5 +13,45 @@ public class AnimalTests<TAnimal> where TAnimal : Animal, new()
         animal.IncreaseHunger();
 
         Assert.That(animal.HungerLevel, Is.GreaterThan(initialHunger));
+    }
+
+    [Test]
+    public void ConsumeFood_When_Called_DecreasesHungerLevel()
+    {
+        TAnimal animal = new TAnimal();
+        int initialHunger = animal.HungerLevel;
+
+        animal.IncreaseHunger();
+        animal.IncreaseHunger();
+
+        animal.ConsumeFood(1);
+
+        Assert.That(animal.HungerLevel, Is.LessThan(initialHunger));
+    }  
+
+    [Test]
+    public void ConsumeFood_WhenCalled_DoesNothingWhenHungerLevelZero()
+    {
+        TAnimal animal = new TAnimal();
+        int initialHunger = animal.HungerLevel;
+
+        animal.ConsumeFood(2);
+
+        Assert.That(initialHunger, Is.EqualTo(animal.HungerLevel));
+        Assert.That(initialHunger, Is.AtLeast(0));
+    }
+
+    [Test]
+    public void ConsumeFood_WhenHungerIsLow_DoesNotGoBelowZero()
+    {
+        TAnimal animal = new TAnimal();
+        int initialHunger = animal.HungerLevel;
+
+        animal.IncreaseHunger();
+        animal.IncreaseHunger();
+
+        animal.ConsumeFood(4);
+
+        Assert.That(animal.HungerLevel, Is.EqualTo(0));
     }
 }
